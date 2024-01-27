d1c4ccfdb79b279148c461e7f24f1db0ce4b0270
GameDirection <Gamedirection@hotmail.com>
2024-01-27 15:56:49 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 info.setScore(0)
 scene.setBackgroundImage(assets.image`cityscape`)
-let mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
-let player2 = sprites.create(assets.image`myImage1`, SpriteKind.Player)
-controller.player1.moveSprite(mySprite, 100, 100)
-controller.player2.moveSprite(player2, 100, 100)
+let MainCharacter = sprites.create(img`
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 b c 5 5 d 4 c . . 
+    . b b b b 5 5 5 b f d d 4 4 4 b 
+    . b d 5 b 5 5 b c b 4 4 4 4 b . 
+    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+    . . b d 5 5 b 5 5 5 5 5 5 b . . 
+    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+    b d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
