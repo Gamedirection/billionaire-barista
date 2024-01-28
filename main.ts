80cbb67d9662895890e68f7bddecd3b4e55ca9d3
pecan4 <157906054+pecan4@users.noreply.github.com>
2024-01-28 16:10:46 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 namespace SpriteKind {
     export const npc = SpriteKind.create()
     export const greyed_npc = SpriteKind.create()
+    export const stop_talking = SpriteKind.create()
+    export const not_food = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        timer_onoff = 1
+        StoryTeller(1)
+        score_multiplier = sprites.readDataNumber(otherSprite, "score multiplier")
+    }
+})
 function StoryTeller (storie: number) {
+    time = 0
     Noun = [
     "map",
     "computer",
@@ -262,41 +272,70 @@ function StoryTeller (storie: number) {
     "Charity Bigbucks"
     ]
     Class = ["Millionaire", "Billionaire", "Trillionaire"]
-    morning_stories = ["I woke up on a " + Adjective._pickRandom() + " morning and got out of my " + Adjective._pickRandom() + " bed. then I went to my " + Adjective._pickRandom() + " bathroom, only to find a " + Adjective._pickRandom() + " " + Noun._pickRandom() + " on the floor! I was so [adjective], but I [adverb] carried on with my day and asked [name] to make me [exotic food]." + Adjective._pickRandom() + ", but I [adverb] carried on with my day and asked [name] to make me [exotic food]." + Adverb._pickRandom() + " carried on with my day and asked [name] to make me [exotic food]." + Names._pickRandom() + " to make me some" + Noun._pickRandom() + ".", "I woke up on a generally " + Adjective._pickRandom() + " day. I was going to go and eat some " + Noun._pickRandom() + " but " + Names._pickRandom() + ", my chef said that he asked for the day off. Of course I was " + Adjective._pickRandom() + " and I " + Adverb._pickRandom() + " fixed my own  breakfast. Then I went upstairs to my room and changed into my " + Adjective._pickRandom() + " " + Noun._pickRandom() + ".", "I woke up on this " + Adjective._pickRandom() + " day and turned on the light to my room. I started to " + Verb._pickRandom() + " down the stairs. Then out of nowhere my " + Noun._pickRandom() + " shouted and I " + Adverb._pickRandom() + " fell down the stairs. And to make things worse, " + Names._pickRandom() + " saw it all happen! I then " + Adverb._pickRandom() + " went and got my breakfast."]
-    lunch_stories = ["At lunch time I was utterly " + Adjective._pickRandom() + ". I decided to go to [place] for some food." + Noun._pickRandom() + " for some food.", "For lunch, I decided to stay in " + "[place]" + " to eat. I ordered some " + Noun._pickRandom() + " and it was [adjective], so i tipped [random # from 5-75] thousand dollars. they were [adjective] with me, but they didn't deserve to look at me, so i walked to my [noun] and left" + Adjective._pickRandom() + ", so i tipped " + randint(5, 75) + " thousand dollars. they were " + Adjective._pickRandom() + " with me, but they didn't deserve to look at me, so i walked to my " + Noun._pickRandom() + " and left."]
+    morning_stories = ["I woke up on a " + Adjective._pickRandom() + " morning and got out of my " + Adjective._pickRandom() + " bed. then I went to my " + Adjective._pickRandom() + " bathroom, only to find a " + Adjective._pickRandom() + " " + Noun._pickRandom() + " on the floor! I was so " + Adjective._pickRandom() + ", but I " + Adverb._pickRandom() + " carried on with my day and asked " + Names._pickRandom() + " to make me some" + Noun._pickRandom() + ". ", "I woke up on a generally " + Adjective._pickRandom() + " day. I was going to go and eat some " + Noun._pickRandom() + " but " + Names._pickRandom() + ", my chef said that he asked for the day off. Of course I was " + Adjective._pickRandom() + " and I " + Adverb._pickRandom() + " fixed my own  breakfast. Then I went upstairs to my room and changed into my " + Adjective._pickRandom() + " " + Noun._pickRandom() + ". ", "I woke up on this " + Adjective._pickRandom() + " day and turned on the light to my room. I started to " + Verb._pickRandom() + " down the stairs. Then out of nowhere my " + Noun._pickRandom() + " shouted and I " + Adverb._pickRandom() + " fell down the stairs. And to make things worse, " + Names._pickRandom() + " saw it all happen! I then " + Adverb._pickRandom() + " went and got my breakfast. "]
+    lunch_stories = ["At lunch time I was utterly " + Adjective._pickRandom() + ". I decided to go to " + Noun._pickRandom() + " for some food." + " for some food. ", "For lunch, I decided to stay in " + Noun._pickRandom() + " to eat. I ordered some " + Noun._pickRandom() + " and it was " + Adjective._pickRandom() + ", so i tipped " + randint(5, 75) + " thousand dollars. they were " + Adjective._pickRandom() + " with me, but they didn't deserve to look at me, so i walked to my " + Noun._pickRandom() + " and left. "]
     fillers = [
-    "My " + Noun._pickRandom() + " decided to " + Verb._pickRandom() + "! It's absolutely " + Adjective._pickRandom() + "! Even my gold-plated " + Noun._pickRandom() + " isn't working. Being a billionaire is tough!",
-    "My diamond-encrusted " + Noun._pickRandom() + " just had to " + Verb._pickRandom() + " today, how " + Adjective._pickRandom() + "! And to top it off, my velvet " + Noun._pickRandom() + " is too " + Adjective._pickRandom() + "! Truly, a hard day.",
-    "My " + Adjective._pickRandom() + " " + Noun._pickRandom() + ", which refused to " + Verb._pickRandom() + ", Even my limited-edition " + Noun._pickRandom() + " is acting " + Adjective._pickRandom() + " today!",
-    "In " + Names._pickRandom() + "'s " + Adjective._pickRandom() + " mansion, their " + Noun._pickRandom() + " suddenly wouldn't " + Verb._pickRandom() + ". 'And now my crystal " + Noun._pickRandom() + " is " + Adjective._pickRandom() + "!' they cried. 'Why must money bring such trials?'",
-    "",
-    ""
+    "My " + Noun._pickRandom() + " decided to " + Verb._pickRandom() + "! It's absolutely " + Adjective._pickRandom() + "! Even my gold-plated " + Noun._pickRandom() + " isn't working. Being a billionaire is tough! ",
+    "My diamond-encrusted " + Noun._pickRandom() + " just had to " + Verb._pickRandom() + " today, how " + Adjective._pickRandom() + "! And to top it off, my velvet " + Noun._pickRandom() + " is too " + Adjective._pickRandom() + "! Truly, a hard day. ",
+    "My " + Adjective._pickRandom() + " " + Noun._pickRandom() + ", which refused to " + Verb._pickRandom() + ", Even my limited-edition " + Noun._pickRandom() + " is acting " + Adjective._pickRandom() + " today! ",
+    "In " + Names._pickRandom() + "'s " + Adjective._pickRandom() + " mansion, their " + Noun._pickRandom() + " suddenly wouldn't " + Verb._pickRandom() + ". 'And now my crystal " + Noun._pickRandom() + " is " + Adjective._pickRandom() + "!' they cried. 'Why must money bring such trials?' "
     ]
-    story.printCharacterText("Hello there! I'm " + Names._pickRandom() + " a " + Class._pickRandom() + " whose life is anything but ordinary. Allow me to recount a day filled with peculiar predicaments that only someone of my wealth could encounter.  Today begins on a" + Adjective._pickRandom() + "morning in my" + Noun._pickRandom() + " mansion, nestled in " + Noun._pickRandom() + " I had planned to wear my " + Adjective._pickRandom() + "suit, but to my " + Noun._pickRandom() + ", I found it " + Noun._pickRandom() + " shrunk by the laundry service. \"This is " + Adjective._pickRandom() + "!\" I exclaimed." + Noun._pickRandom() + " For breakfast, I fancied some " + Noun._pickRandom() + ", but my chef, " + Names._pickRandom() + ", informed me that the " + Noun._pickRandom() + " was broken. \"Preposterous!\" I cried, opting for a mundane " + Noun._pickRandom() + ". Seeking solace, I decided to visit my " + Noun._pickRandom() + " via my " + Noun._pickRandom() + ". Alas, my " + Noun._pickRandom() + " informed me of a " + Adjective._pickRandom() + ". \"This is utterly " + Noun._pickRandom() + "!\" I grumbled. " + Noun._pickRandom() + " Feeling " + Noun._pickRandom() + ", I thought a bit of shopping might help. I desired a " + Noun._pickRandom() + ", but upon reaching " + Noun._pickRandom() + ", I discovered they had just sold the last one. \"How " + Noun._pickRandom() + "!\" I lamented. Lunch was no better. I craved " + Noun._pickRandom() + ", but " + Noun._pickRandom() + " told me we were out of " + Noun._pickRandom() + "!\" I sighed, settling for " + Noun._pickRandom() + ". The afternoon brought its own trials. I wanted to relax in my " + Noun._pickRandom() + ", but " + Noun._pickRandom() + " reported a " + Noun._pickRandom() + " with it. \"Is this a " + Noun._pickRandom() + " joke?\" I wondered. Come dinner, I longed for " + Noun._pickRandom() + ", but a " + Noun._pickRandom() + " made it impossible. \"This is beyond " + Noun._pickRandom() + "!\" I declared, choosing " + Noun._pickRandom() + "instead.  As I retired to my " + Noun._pickRandom() + ", I couldn't help but chuckle at the day's " + Noun._pickRandom() + " mishaps. Despite everything, I knew tomorrow would bring more " + Noun._pickRandom() + " adventures in my life as a " + Class._pickRandom() + ".", Names._pickRandom())
+    dinnerend_stories = ["as I ate some food and retired to my room, I couldn't help but chuckle at the day's " + Adjective._pickRandom() + " mishaps. Despite everything, I knew tomorrow would bring more " + Adjective._pickRandom() + " adventures in my life as a " + Class._pickRandom() + "."]
+    if (Math.percentChance(1)) {
+        story.printCharacterText("Hello there! I'm " + "" + " a " + Class._pickRandom() + " whose life is anything but ordinary. Allow me to recount a day filled with peculiar predicaments that only someone of my wealth could encounter.  Today begins on a" + Adjective._pickRandom() + "morning in my" + Noun._pickRandom() + " mansion, nestled in " + Noun._pickRandom() + " I had planned to wear my " + Adjective._pickRandom() + "suit, but to my " + Noun._pickRandom() + ", I found it " + Noun._pickRandom() + " shrunk by the laundry service. \"This is " + Adjective._pickRandom() + "!\" I exclaimed." + Noun._pickRandom() + " For breakfast, I fancied some " + Noun._pickRandom() + ", but my chef, " + Names._pickRandom() + ", informed me that the " + Noun._pickRandom() + " was broken. \"Preposterous!\" I cried, opting for a mundane " + Noun._pickRandom() + ". Seeking solace, I decided to visit my " + Noun._pickRandom() + " via my " + Noun._pickRandom() + ". Alas, my " + Noun._pickRandom() + " informed me of a " + Adjective._pickRandom() + ". \"This is utterly " + Noun._pickRandom() + "!\" I grumbled. " + Noun._pickRandom() + " Feeling " + Noun._pickRandom() + ", I thought a bit of shopping might help. I desired a " + Noun._pickRandom() + ", but upon reaching " + Noun._pickRandom() + ", I discovered they had just sold the last one. \"How " + Noun._pickRandom() + "!\" I lamented. Lunch was no better. I craved " + Noun._pickRandom() + ", but " + Noun._pickRandom() + " told me we were out of " + Noun._pickRandom() + "!\" I sighed, settling for " + Noun._pickRandom() + ". The afternoon brought its own trials. I wanted to relax in my " + Noun._pickRandom() + ", but " + Noun._pickRandom() + " reported a " + Noun._pickRandom() + " with it. \"Is this a " + Noun._pickRandom() + " joke?\" I wondered. Come dinner, I longed for " + Noun._pickRandom() + ", but a " + Noun._pickRandom() + " made it impossible. \"This is beyond " + Noun._pickRandom() + "!\" I declared, choosing " + Noun._pickRandom() + "instead.  As I retired to my " + Noun._pickRandom() + ", I couldn't help but chuckle at the day's " + Noun._pickRandom() + " mishaps. Despite everything, I knew tomorrow would bring more " + Noun._pickRandom() + " adventures in my life as a " + Class._pickRandom() + ".")
+        info.changeScoreBy(2 * (time * Math.round(sprites.readDataNumber(customer, "score multiplier"))))
+    } else {
+        story.printCharacterText("" + morning_stories._pickRandom() + fillers._pickRandom() + lunch_stories._pickRandom() + fillers._pickRandom() + dinnerend_stories._pickRandom(), Names._pickRandom())
+    }
+    story.printCharacterText("cya")
+    info.changeScoreBy(time * score_multiplier)
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    StoryTeller(1)
-})
 function place_character_at_spot (spot: number) {
-    customer = sprites.create(assets.image`myImage2`, SpriteKind.npc)
+    random = randint(0, 3)
+    if (random == 0) {
+        customer = sprites.create(assets.image`char5`, SpriteKind.npc)
+    } else if (random == 1) {
+        customer = sprites.create(assets.image`char2`, SpriteKind.npc)
+    } else if (random == 2) {
+        customer = sprites.create(assets.image`char4`, SpriteKind.npc)
+    } else if (random == 3) {
+        customer = sprites.create(assets.image`char1`, SpriteKind.npc)
+    }
     if (spot == 0) {
         tiles.placeOnTile(customer, tiles.getTileLocation(2, 5))
         customer.y += -3
     } else if (spot == 1) {
         tiles.placeOnTile(customer, tiles.getTileLocation(10, 5))
-        customer.y += -3
         customer.x += -2
     } else if (spot == 2) {
         tiles.placeOnTile(customer, tiles.getTileLocation(14, 5))
         customer.y += -3
-    } else {
-    	
+    }
+    sprites.setDataNumber(customer, "score multiplier", 1)
+    for (let index = 0; index < randint(1, 9); index++) {
+        sprites.changeDataNumberBy(customer, "score multiplier", 0.1)
     }
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-	
+sprites.onOverlap(SpriteKind.Player, SpriteKind.stop_talking, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        story.clearAllText()
+        sprite.x += 20
+        timer_onoff = 0
+        time = 0
+        for (let value of sprites.allOfKind(SpriteKind.npc)) {
+            sprites.destroy(value)
+        }
+        place_character_at_spot(1)
+    }
 })
+spriteutils.onSpriteKindUpdateInterval(SpriteKind.npc, 5000, function (sprite) {
+    if (sprites.readDataNumber(sprite, "score multiplier") >= 1.5) {
+        sprite.startEffect(effects.smiles, 500)
+    }
+})
+let random = 0
 let customer: Sprite = null
+let dinnerend_stories: string[] = []
 let fillers: string[] = []
 let lunch_stories: string[] = []
 let morning_stories: string[] = []
@@ -305,9 +344,15 @@ let Names: string[] = []
 let Adverb: string[] = []
 let Adjective: string[] = []
 let Noun: string[] = []
+let time = 0
+let score_multiplier = 0
+let timer_onoff = 0
 let Verb: string[] = []
+scene.setBackgroundImage(assets.image`myImage3`)
+pauseUntil(() => controller.A.isPressed())
+pause(100)
 info.setScore(0)
-scene.setBackgroundImage(assets.image`cityscape0`)
+scene.setBackgroundImage(assets.image`backdrop`)
 tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`level1`))
 Verb = ["This is Message A", "This is Message B", "This is Message C"]
 let cursor = sprites.create(img`
@@ -331,3 +376,26 @@ let cursor = sprites.create(img`
 controller.moveSprite(cursor)
 cursor.z = 30
 place_character_at_spot(1)
+let mySprite = sprites.create(assets.image`question`, SpriteKind.stop_talking)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 1))
+info.startCountdown(480)
+let foodsprite = sprites.create(assets.image`cookieBut`, SpriteKind.Food)
+tiles.placeOnTile(foodsprite, tiles.getTileLocation(3, 12))
+foodsprite.z = 5
+foodsprite.x += 2
+foodsprite.y += 2
+foodsprite = sprites.create(assets.image`cookieBut0`, SpriteKind.Food)
+foodsprite.z = 6
+tiles.placeOnTile(foodsprite, tiles.getTileLocation(9, 12))
+foodsprite.x += 4
+foodsprite.y += 2
+foodsprite = sprites.create(assets.image`cookieBut1`, SpriteKind.Food)
+foodsprite.z = 7
+tiles.placeOnTile(foodsprite, tiles.getTileLocation(16, 12))
+foodsprite.x += -2
+foodsprite.y += 2
+let notfoodsprite = sprites.create(assets.image`counter`, SpriteKind.not_food)
+notfoodsprite.setPosition(80, 100)
+game.onUpdateInterval(1000, function () {
+    time += 1
+})
